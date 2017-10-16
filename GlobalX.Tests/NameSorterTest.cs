4dc069181fe776245d182b9c032666e1306c01b4
beau <byarranton@hotmail.com>
2017-10-16 23:06:42 +1000

    Add project files.

---
+++ GlobalX.Tests/NameSorterTest.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Moq;
+using System.Collections.Generic;
+
+namespace GlobalX.Tests
+{
+    /// <summary>
+    /// Class to test the NameSorter class
+    /// </summary>
+    [TestClass]
+    public class NameSorterTest
+    {
+        [TestMethod]
+        public void TestMain()
+        {
+            //Main method is not tested
+            Assert.AreEqual(1, 1);
+        }
+
+        /// <summary>
+        /// Test the class constructor with a file that contains a line which doesn't have a surname (only has one name).
+        /// Assert the constructor raises an InvalidDataException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void Instantiate_with_file_containing_one_line_with_no_surname()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("Jason Jay Derulo")
+                      .Returns("Beyonce")
+                      .Returns("Jay Z");
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+        }
+
+        /// <summary>
+        /// Test the class constructor with a file that contains a blank line.
+        /// Assert the constructor raises an InvalidDataException.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidDataException))]
+        public void Instantiate_with_file_containing_blank_line()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("Jason Jay Derulo")
+                      .Returns("")
+                      .Returns("Jay Z");
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+        }
+
+        /// <summary>
+        /// Test the class constructor with an empty input file.
+        /// Assert the instance is created successfully.
+        /// </summary>
+        [TestMethod]
+        public void Instantiate_with_emtpy_file()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns(null);
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+
+            //Assert an instance is created
+            Assert.IsInstanceOfType(ns, typeof(NameSorter));
+            Assert.IsNotNull(ns);
+        }
+
+        /// <summary>
+        /// Test the class constructor with an input file containing one line.
+        /// Assert the instance is created successfully.
+        /// </summary>
+        [TestMethod]
+        public void Instantiate_with_one_line_input()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("Valid Girl");
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+
+            //Assert an instance is created
+            Assert.IsInstanceOfType(ns, typeof(NameSorter));
+            Assert.IsNotNull(ns);
+        }
+
+        /// <summary>
+        /// Test the GivenNamesFirst method with an empty names list.
+        /// Assert the returned list is an empty array.
+        /// </summary>
+        [TestMethod]
+        public void Test_GivenNamesFirst_with_empty_list()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns(null);
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            Assert.AreEqual(0, actualResult.Count);
+        }
+
+        /// <summary>
+        /// Test the GivenNamesFirst method with a single-name list.
+        /// Assert the returned list contains the name.
+        /// </summary>
+        [TestMethod]
+        public void Test_GivenNamesFirst_with_single_entry()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("The Only Person");
+            List<string> expectedResult = new List<string> { "The Only Person" };
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Test the GivenNamesFirst method with a two-name list.
+        /// Assert the returned list contains the two names.
+        /// </summary>
+        [TestMethod]
+        public void Test_GivenNamesFirst_with_two_entries()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("Jackson The Pollock")
+                      .Returns("Leonardo DaVinci");
+            List<string> expectedResult = new List<string> {
+                "Jackson The Pollock", "Leonardo DaVinci" };
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Test the SortBySurname method with an empty names list.
+        /// Assert no error.
+        /// </summary>
+        [TestMethod]
+        public void Test_SortBySurname_with_empty_list()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns(null);
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            ns.SortBySurname();
+        }
+
+        /// <summary>
+        /// Test the SortBySurname method with a single-name list.
+        /// Assert no error.
+        /// </summary>
+        [TestMethod]
+        public void Test_SortBySurname_with_single_entry()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("The Only Person");
+            List<string> expectedResult = new List<string> { "The Only Person" };
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            ns.SortBySurname();
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Test the SortBySurname method with a multi-name list.
+        /// Assert the returned list is correctly sorted.
+        /// </summary>
+        [TestMethod]
+        public void Test_SortBySurname_with_multi_entries()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("Alpha Jerry Beta")
+                      .Returns("Alpha Yiotta")
+                      .Returns("Zeta Aak")
+                      .Returns("Barry Beta")
+                      .Returns("Alpha Beta");
+            List<string> expectedResult = new List<string> {
+                "Zeta Aak", "Alpha Beta", "Alpha Jerry Beta", "Barry Beta", "Alpha Yiotta" };
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            ns.SortBySurname();
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Test the SortByGivenNames method with an empty names list.
+        /// Assert no error.
+        /// </summary>
+        [TestMethod]
+        public void Test_SortByGivenNames_with_empty_list()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns(null);
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            ns.SortByGivenNames();
+        }
+
+        /// <summary>
+        /// Test the SortByGivenNames method with a single-name list.
+        /// Assert no error.
+        /// </summary>
+        [TestMethod]
+        public void Test_SortByGivenNames_with_single_entry()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("The Only Person");
+            List<string> expectedResult = new List<string> { "The Only Person" };
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            ns.SortByGivenNames();
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Test the SortByGivenNames method with a multi-name list.
+        /// Assert the returned list is correctly sorted.
+        /// </summary>
+        [TestMethod]
+        public void Test_SortByGivenNames_with_multi_entries()
+        {
+            //Arrange (Mock the TextReader)
+            var textReader = new Mock<TextReader>();
+            textReader.SetupSequence(r => r.ReadLine())
+                      .Returns("Alpha Jerry Beta")
+                      .Returns("Alpha Yiotta")
+                      .Returns("Zeta Aak")
+                      .Returns("Barry Beta")
+                      .Returns("Alpha Beta");
+            List<string> expectedResult = new List<string> {
+                "Alpha Beta", "Alpha Yiotta", "Alpha Jerry Beta", "Barry Beta", "Zeta Aak" };
+
+            //Act
+            NameSorter ns = new NameSorter(textReader.Object);
+            ns.SortByGivenNames();
+            List<string> actualResult = ns.GivenNamesFirst();
+
+            //Assert
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ GlobalX/NameSorter.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalX
+{
+    /// <summary>
+    /// Class to Read names from a .txt file and perform sorting and writing operations
+    /// </summary>
+    public class NameSorter
+    {
+        //List of names as read from the names file (given names are first)
+        private List<string> names;
+
+        /// <summary>
+        /// Reads the names from the file given in args. Sorts the names by surname then given names; saves the sorted list to another file; and prints sorted list to console.
+        /// </summary>
+        /// <remarks>
+        /// Usage:  name-sorter [input_file_path]
+        /// </remarks>
+        /// <param name="args">The filepath to the file containing the names. The file is a .txt file. Each line in the file has the form "given_names surname", and can have up to 3 given names.
+        /// </param>
+        static void Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("ERROR: No input file was specified");
+                return;
+            }
+
+            //Read the file path from the input argument
+            string inputFile = args[0];
+
+            //Check file exists
+            if (!File.Exists(inputFile))
+            {
+                Console.WriteLine("ERROR: " + inputFile + " not found");
+                Console.ReadLine();
+                return;
+            }
+
+            //Create a StreamReader
+            TextReader textReader = new StreamReader(inputFile);
+
+            //Instantiate the class
+            try
+            {
+                NameSorter ns = new NameSorter(textReader);
+
+                //Sort the names by surname
+                ns.SortBySurname();
+
+                //File to write to
+                string outFile = "sorted-names-list.txt";
+
+                //Write the sorted names to file
+                ns.WriteToFile(outFile);
+
+                //Get the sorted names
+                List<string> sortedNames = ns.GivenNamesFirst();
+
+                //Print sorted names to console
+                foreach (string name in sortedNames)
+                {
+                    Console.WriteLine(name);
+                }
+                Console.ReadLine();
+            }
+            catch (InvalidDataException e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+                return;
+            }
+            finally
+            {
+                textReader.Close();
+            }
+        }
+
+        /// <summary>
+        /// Class constructor. Reads the names from file and puts them into givennames_surnames
+        /// </summary>
+        /// <param name="textReader">TextReader pointing to the file containing the unsorted names
+        /// </param>
+        /// <exception cref="InvalidDataException">If fileName contains invalid formatting</exception>
+        public NameSorter(TextReader textReader)
+        {
+            //Create the names list
+            names = new List<string>();
+
+            //Current line being read
+            string line;
+
+            //Current line count
+            int lineCount = 1;
+
+            //Read line at a time
+            while ((line = textReader.ReadLine()) != null)
+            {
+                //Check the line contains at least two strings (surname and one given name)
+                if (!line.Trim().Contains(" "))
+                {
+                    throw new InvalidDataException("All lines must contain a surname and at least one given name (error on line " + lineCount + ")");
+                }
+
+                //Remove leading/trailing whitespace and add to names list
+                names.Add(line.Trim());
+
+                lineCount++;
+            }
+        }
+
+        /// <summary>
+        /// Returns the list of names, where each element of the list has form "given_names surname". List has whatever order the current instance has. 
+        /// </summary>
+        /// <returns>
+        /// list of name strings where each element has form "given_names surnames"
+        /// </returns>
+        public List<string> GivenNamesFirst()
+        {
+            return names;
+        }
+
+        /// <summary>
+        /// Sorts the list of names, first by surname, then by any given names the person may have.
+        /// </summary>
+        public void SortBySurname()
+        {
+            //Use delegate to sort by surnames and then by given names
+            names.Sort(delegate (string p1, string p2)
+            {
+                //Get surnames for p1 and p2
+                string p1surname = p1.Trim().Split(' ').Last();
+                string p2surname = p2.Trim().Split(' ').Last();
+
+                //Get given names for p1 and p2
+                string p1givenNames = p1.Replace(p1surname, "");
+                string p2givenNames = p2.Replace(p2surname, "");
+
+                if (p1surname.Equals(p2surname))
+                {
+                    //Then can't separate by surnames, so sort by given names
+                    return p1givenNames.CompareTo(p2givenNames);
+                }
+                //Sort by surnames
+                return p1surname.CompareTo(p2surname);
+            });
+        }
+
+        /// <summary>
+        /// Sorts the list of names, first by given names, then by surname.
+        /// </summary>
+        public void SortByGivenNames()
+        {
+            //Use delegate to sort by given names and then by surname
+            names.Sort(delegate (string p1, string p2)
+            {
+                //Get surnames for p1 and p2
+                string p1surname = p1.Trim().Split(' ').Last();
+                string p2surname = p2.Trim().Split(' ').Last();
+
+                //Get given names for p1 and p2
+                string p1givenNames = p1.Replace(p1surname, "");
+                string p2givenNames = p2.Replace(p2surname, "");
+
+                if (p1givenNames.Equals(p2givenNames))
+                {
+                    //Then can't separate by given names, so sort by surnames
+                    return p1surname.CompareTo(p2surname);
+                }
+                //Sort by given names
+                return p1givenNames.CompareTo(p2givenNames);
+            });
+        }
+
+        /// <summary>
+        /// Writes the names list to file (in whatever the current order of the instance is).
+        /// </summary>
+        /// <param name="filepath">
+        ///  string giving the filepath of the .txt file to write to.
+        /// </param>
+        public void WriteToFile(string filepath)
+        {
+            File.WriteAllLines(filepath, names);
+        }
+    }
+}
